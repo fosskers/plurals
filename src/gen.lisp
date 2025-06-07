27a13702dd153696f67844db177d6e947def1ac1
Colin Woodbury <colin@fosskers.ca>
2025-06-07 09:00:22 +0900

    [gen] rule expression parsers

--- src/gen.lisp
+++ src/gen.lisp
@@ -12,6 +12,8 @@
 ;; default to the `other' line of the translation (or the line marked as default
 ;; if `other' is missing).
 
+(defparameter +skip-space+ (p:consume (lambda (c) (or (equal c #\space) (equal c #\tab)))))
+
 (defun string->keyword (s)
   (intern (string-upcase s) "KEYWORD"))
 
@@ -30,15 +32,125 @@ with `parcom'."
   (cat  nil :type keyword)
   (rule nil :type string))
 
-;; TODO: Start here. The string we're attempting to parse isn't a simple-string.
 (defun element->rule (el)
-  (make-rule :rule (->> (x:content el) string->simple parse-rule)
+  (make-rule :rule (->> (x:content el) string->simple p-rule)
              :cat (->> (x:element-metadata el)
                        (gethash "count")
                        (string->keyword))))
 
-(defun parse-rule (s)
-  (p:parse (p:take-while (lambda (c) (not (eql c #\@)))) s))
+(defun p-rule (s)
+  (p:parse (<* #'p-condition +skip-space+ #'p-example) s))
+
+#+nil
+(p-rule "n = 1 @integer")
+
+(defun p-example (offset)
+  (funcall (*> (p:char #\@)
+               (p:consume #'identity))
+           offset))
+
+(defun p-condition (offset)
+  (p:fmap (lambda (list)
+            (if (null (cdr list))
+                (car list)
+                (cons :or list)))
+          (funcall (p:sep1 (*> +skip-space+
+                               (p:string "or")
+                               +skip-space+)
+                           #'p-and-condition)
+                   offset)))
+
+#+nil
+(p:parse #'p-condition "i = 1")
+#+nil
+(p:parse #'p-condition "i = 1 or v = 2")
+#+nil
+(p:parse #'p-condition "i = 1 and n = 3 or v = 2")
+
+(defun p-and-condition (offset)
+  (p:fmap (lambda (list)
+            (if (null (cdr list))
+                (car list)
+                (cons :and list)))
+          (funcall (p:sep1 (*> +skip-space+
+                               (p:string "and")
+                               +skip-space+)
+                           #'p-relation)
+                   offset)))
+
+#+nil
+(p:parse #'p-and-condition "i = 1")
+#+nil
+(p:parse #'p-and-condition "i = 1 and v = 0")
+
+(defun p-relation (offset)
+  (p:fmap (lambda (list)
+            (destructuring-bind (lhs op rhs) list
+              (list op lhs rhs)))
+          (funcall (<*> #'p-expr
+                        (*> +skip-space+ (p:alt (<$ :eq  (p:char #\=))
+                                                (<$ :neq (p:string "!="))))
+                        (*> +skip-space+ #'p-range-list))
+                   offset)))
+
+#+nil
+(p:parse #'p-relation "i = 1")
+#+nil
+(p:parse #'p-relation "i = 1,2")
+
+(defun p-expr (offset)
+  (p:fmap (lambda (pair)
+            (if (cadr pair)
+                (destructuring-bind (op (f v)) pair
+                  (list f op v))
+                (car pair)))
+          (funcall (<*> #'p-operand
+                        (*> +skip-space+
+                            (p:opt (<*> (<$ :mod (p:char #\%))
+                                        (*> +skip-space+ #'p:unsigned)))))
+                   offset)))
+
+#+nil
+(p:parse #'p-expr "i % 10")
+#+nil
+(p:parse #'p-expr "i")
+
+(defun p-operand (offset)
+  (funcall (p:alt (<$ :n (p:char #\n))
+                  (<$ :i (p:char #\i))
+                  (<$ :v (p:char #\v))
+                  (<$ :w (p:char #\w))
+                  (<$ :f (p:char #\f))
+                  (<$ :t (p:char #\t))
+                  (<$ :c (p:char #\c))
+                  (<$ :e (p:char #\e)))
+           offset))
+
+#+nil
+(p:parse #'p-operand "n = 1")
+
+(defun p-range-list (offset)
+  (p:fmap (lambda (list)
+            (if (null (cdr list))
+                (car list)
+                list))
+          (funcall (p:sep1 (p:char #\,) (p:alt #'p-range #'p:unsigned))
+                   offset)))
+
+#+nil
+(p:parse #'p-range-list "17")
+#+nil
+(p:parse #'p-range-list "0..1,3..5")
+
+(defun p-range (offset)
+  (p:fmap (lambda (pair) (cons :range pair))
+          (funcall (<*> #'p:unsigned
+                        (*> (p:string "..")
+                            #'p:unsigned))
+                   offset)))
+
+#+nil
+(p:parse #'p-range "0..1")
 
 (defun rules-by-locale (xml)
   "Parse some XML and recollate all plural rules into a single Hash Table, keyed by
